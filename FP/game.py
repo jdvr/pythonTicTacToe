0dc25a2e0a7c9957a04f59860d05dae05283904c
Juan <jdvr.93@hotmail.com>
2014-06-09 10:08:15 +0100

    win method undefined

--- FP/game.py
+++ FP/game.py
@@ -1,3 +1,4 @@
+from state import State
 
 
 class Game(object):
@@ -21,4 +22,9 @@ class Game(object):
         new_state = self.__state.put_symbol(self.__player_one.get_type(), row, column) if self.__player_one_turn else \
             self.__state.put_symbol(self.__player_two.get_type(), row, column)
         return Game(new_state, self.__player_one, self.__player_two)
+
+    def cross_win(self):
+        return self.vertical_win(State.X) or self.horizontal_win(State.X) or self.diagonal_win(State.X)
+
+
 
--- FP/test_game.py
+++ FP/test_game.py
@@ -3,6 +3,7 @@ from state import State
 from player import Player
 from game import Game
 
+
 class TestGame(unittest.TestCase):
 
     def setUp(self):
@@ -21,6 +22,19 @@ class TestGame(unittest.TestCase):
     def test_changing_player_turn(self):
         game = Game(self.__state, self.__player_o, self.__player_x)
         self.assertEqual(game.change_turn().get_player_turn(), self.__player_x.get_type())
+
+    def test_cross_win_the_game_horizontal(self):
+        game = Game(self.__state, self.__player_x, self.__player_o)
+        game = game.player_move(0, 0).player_move(0, 1).player_move(0, 2)
+        self.assertTrue(game.crossWin())
+
+    def test_cross_win_the_game_vertical(self):
+        game = Game(self.__state, self.__player_x, self.__player_o)
+        game = game.player_move(0, 0).player_move(1, 0).player_move(2, 0)
+        self.assertTrue(game.cross_win())
+
+
+
 
 
 
